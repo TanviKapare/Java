f47274af79fa1e42b6525c88498c36c24224c11e
TanviKapare <tanvi.kapare@gmail.com>
2026-09-21 11:30:13 -0700

    Java File

---
+++ Student.java
@@ -0,0 +1,15 @@
+package basicJavaPrograms;
+
+public class Student {
+	
+	String name;
+	int roll_no;
+
+	public static void main(String[] args) {
+		//Student s = new Student();
+		
+		
+
+	}
+
+}
